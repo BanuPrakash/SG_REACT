da342d391a498384d23c901935e547247114f655
Banuprakash <banuprakashc@yahoo.co.in>
2025-10-10 12:54:02 +0530

    react-router-dom

--- shopapp/src/features/common/Navbar.jsx
+++ shopapp/src/features/common/Navbar.jsx
@@ -1,11 +1,7 @@
 import React from 'react'
 import AppBar from '@mui/material/AppBar';
-
 import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-
-import MenuIcon from '@mui/icons-material/Menu';
 import { Link } from 'react-router-dom';
 
 
