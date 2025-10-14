01f2077dbbbf06f7ca296d7c4790dbe061072326
Banuprakash <banuprakashc@yahoo.co.in>
2025-10-14 11:06:53 +0530

    redux

--- workspace/reduxexample/src/App.jsx
+++ workspace/reduxexample/src/App.jsx
@@ -2,14 +2,32 @@ import { connect } from "react-redux";
 
 // props coming from connect
 function App(props) {
+   
+  // uncontrolled component
+    // function handleSubmit(evt) {
+      // evt.preventDefault()
+      // var formData = new FormData(evt.target);
+    
+    function handleSubmit(formData) {
+      let contact = {
+        name: formData.get("name"),
+        email: formData.get("email")
+      }
+      props.addContact(contact);
+    }
     return (<div>
       <h1>
         Welcome {props.display}
       </h1>
+      <form action={handleSubmit}>
+        Name <input type="text" name="name" /> <br />
+        Email <input type="email" name="email" /> <br />
+        <button type="submit">Add Contact</button>
+      </form>
       {
           props.contactList.map(contact => <div key={contact.email}>
               {contact.name}, {contact.email} &nbsp;
-              <button type="button">Delete</button>
+              <button type="button" onClick={() => props.removeContact(contact.email)}>Delete</button>
           </div>)
         }
     </div>)
@@ -17,13 +35,16 @@ function App(props) {
 
 function mapStateToProps(state) {
   return {
-    display: state => state.profile.name,
-    contactList: state => state.contacts
+    display:  state.profile.name,
+    contactList:  state.contacts
   }
 }
 
 function mapDispatchToProps(dispatch) {
-
+  return {
+    addContact: contact => dispatch({type:'ADD_CONTACT', payload: contact}),
+    removeContact: email => dispatch({type: 'REMOVE_CONTACT', payload: email})
+  }
 }
 export default connect(
   mapStateToProps,
